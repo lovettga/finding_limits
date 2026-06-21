cc7d40ff3858f5b8aad0d5f96bdbfb85c170f54e
Gabrielle Lovett <86375126+lovettga@users.noreply.github.com>
2026-06-21 00:29:14 -0400

    trying sympy library

--- app.py
+++ app.py
@@ -1,24 +1,41 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sympy as sp
+#from sympy import simplify, symbols, limit, sqrt
 
+# randomize c and b
+
+
+# such that: (x+1) / (x^2+cx+b) = 0 at x=-1
+# limit must simplify to 1/a
+
+
+
+
+x = sp.symbols('x')
+expr = sp.sqrt(  (x+1) / ( (x)**2 + 11*x + 10 ) )
+result = sp.limit(expr, x, -1)
+
+
+# page header
 st.header('Finding Limits')
 
 # create string to display limit using randomized variables
-lim_disp = r'\lim_{x \to %s} \sqrt{ \dfrac{x+%s}{x^{2}+%sx+%s} + 5y - 8n}' % ("1", "2", "3", "4")
+lim_disp = r'\lim_{x \to {-1}} \sqrt{ \dfrac{x+1}{x^{2}+%sx+%s} + 5y - 8n}' % ("1", "2")
 
+# create main container for the question prompt and answer box
 main_container = st.container(border=True, horizontal_alignment="center")
 main_container.markdown("Solve the limit:", text_alignment="center")
 main_container.latex(lim_disp, width="content")
 
 #create 2 columns (centered on the screen) for "your answer:" and input box
-left, right = main_container.columns(2, vertical_alignment="center")
-
+left, right = main_container.columns(2, border=True, vertical_alignment="bottom")
 left.markdown("Your answer: ", text_alignment="right")
 number = right.number_input(label="", value=None, placeholder="Ex: 3.4", width=220)
 
-#input_container.markdown("Your answer: ")
-#number = input_container.number_input(label="", value=0.0, placeholder="Ex: 3.4", width=220)
+
+st.write(f"The limit is: {result}")
 
 st.write("Provided answer: ", number)
 
