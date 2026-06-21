2ad992b15c3657e360c80a7a36ffd09d732e608e
Gabrielle Lovett <86375126+lovettga@users.noreply.github.com>
2026-06-20 23:58:18 -0400

    columns for the answer segment

--- app.py
+++ app.py
@@ -11,10 +11,14 @@ main_container = st.container(border=True, horizontal_alignment="center")
 main_container.markdown("Solve the limit:", text_alignment="center")
 main_container.latex(lim_disp, width="content")
 
+#create 2 columns (centered on the screen) for "your answer:" and input box
+left, right = st.columns(3, vertical_alignment="bottom")
 
-input_container = st.container(border=True, horizontal=True, horizontal_alignment="distribute")
-input_container.markdown("Your answer: ")
-number = input_container.number_input(label="", value=0.0, placeholder="Ex: 3.4", width=220)
+left.markdown("Your answer: ")
+number = right.number_input(label="", value=0.0, placeholder="Ex: 3.4", width=220)
+
+#input_container.markdown("Your answer: ")
+#number = input_container.number_input(label="", value=0.0, placeholder="Ex: 3.4", width=220)
 
 st.write("Provided answer: ", number)
 
