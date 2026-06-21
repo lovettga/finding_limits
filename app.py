495fa75fa7ee00716661ace4340291d2a39b9e4c
Gabrielle Lovett <86375126+lovettga@users.noreply.github.com>
2026-06-20 23:03:28 -0400

    First layout test

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ import pandas as pd
 import numpy as np
 
 st.title('Finding Limits')
+
+
+container = st.container(border=True, horizontal_alignment="center")
+container.write("Solve the limit:")
+container.write("Enter Latex here to display limit")
+number = container.number_input(
+    "Your answer", value=None, placeholder="Type a number..."
+)
+st.write("This is outside the container")
+
