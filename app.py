220cbf5b3feffa2acb0ff17022cbd9539dc36115
Gabrielle Lovett <86375126+lovettga@users.noreply.github.com>
2026-06-20 23:12:07 -0400

    minor string to int fix

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ container = st.container(border=True, horizontal_alignment="center")
 container.markdown("Solve the limit:", text_alignment="center")
 container.write("x**2 + 3*x - 1/2")
 number = container.number_input(
-    "Your answer", value=None, placeholder="Ex: 300", width="200"
+    "Your answer", value=None, placeholder="Ex: 300", width=200
 )
 st.write("This is outside the container")
 
