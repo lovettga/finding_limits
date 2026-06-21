8d36f91c3e69c31a3c780e02fc64b6665235fa6b
Gabrielle Lovett <86375126+lovettga@users.noreply.github.com>
2026-06-20 23:28:42 -0400

    raw string for latex

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import numpy as np
 
 st.header('Finding Limits')
 
+raw_latex = r'\lim_{x \to %s} \sqrt{ \dfrac{x+%s}{x^{2}+%sx+%s} }' % ("1", "2", "3", "4")
 
 container = st.container(border=True, horizontal_alignment="center")
 container.markdown("Solve the limit:", text_alignment="center")
-container.latex("\lim_{x \to %s} \sqrt{ \dfrac{x+%s}{x^{2}+%sx+%s} }" % ("1", "2", "3", "4"), width="content")
+container.latex(raw_latex, width="content")
 number = container.number_input(
     "Your answer", value=None, placeholder="Ex: 300", width=200
 )
