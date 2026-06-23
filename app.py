3bad54957c7dcb6ce59bf12fefbc674be69c1c48
Gabrielle Lovett <86375126+lovettga@users.noreply.github.com>
2026-06-22 22:23:12 -0400

    code cleanup

--- app.py
+++ app.py
@@ -10,13 +10,12 @@ from sympy import nsimplify, symbols, limit, sqrt
 def generate_limit():
     # c and b are dependent on randomizing a
     a = np.random.randint(low=1, high=20)
-    
-    # utilized AI Generated code to generate a limit problem
+
     # c and b were derived and generated as follows:
     c = a**2 + 2
     b = a**2 + 1
     
-    # this works to solve the limit using the generated c and b values
+    # solve the limit using the generated c and b values
     x = symbols('x')
     expr = sqrt(  (x+1) / ( (x)**2 + c*x + b ) )
     result = limit(expr, x, -1)
@@ -25,16 +24,16 @@ def generate_limit():
 
 # Handle the callback for the answer box
 def handle_text_change():
-    # Access the text using the widget's unique key
+    # get text string from answer box
     current_text = st.session_state.answer_area
     
-    # DO SOMETHING WITH THE ANSWER, LIKE DETERMINE IF IT IS CORRECT OR NOT :)
-    # convert provided answer into a fraction and them compare against correct answer
+    # convert provided answer into a fraction
     # prevent error edge case if submit is pressed with no answer input
     if current_text is not None:
         current_answer = nsimplify(current_text)
     else:
         return
+    # compare provided answer against correct answer
     answer = False
     if current_answer == st.session_state.result:
         answer = True
@@ -43,13 +42,13 @@ def handle_text_change():
     st.session_state.answer = answer
     st.session_state.curr_answer = current_answer
 
-# Clear all session_state keys for application reset
+# Clear all session_state keys for question reset
 def clear_session_state():
-    # Delete all items from the Session State dictionary
+    # delete all items from the session state, app auto reruns
     for key in list(st.session_state.keys()):
         del st.session_state[key]
 
-# toggles value used to show/hide content (use case is for solution display)
+# toggles value used for solution display button
 def toggle_content():
     st.session_state.show_content = not st.session_state.show_content
 
@@ -72,7 +71,7 @@ st.header('Finding Limits')
 # create string to display limit using randomized variables in LaTeX
 lim_disp = r'\lim\limits_{x \to -1} \sqrt{ \dfrac{x+1}{x^{2}+%sx+%s} }' % (st.session_state.c, st.session_state.b)
 
-# create main container to hold the question prompt
+# create main container to hold the entire question prompt
 main_container = st.container(border=True, horizontal_alignment="center")
 main_container.markdown("## Solve the limit:", text_alignment="center")
 main_container.markdown(f"${lim_disp}$", text_alignment="center")
@@ -113,7 +112,7 @@ st.button("Try Another", on_click=clear_session_state)
 # initialize in session_state
 if "show_content" not in st.session_state:
     st.session_state.show_content = False
-# determine if solution should be shown
+# determine button label to be shown
 if st.session_state.show_content: 
     button_label = "Hide Solution" 
 else:
